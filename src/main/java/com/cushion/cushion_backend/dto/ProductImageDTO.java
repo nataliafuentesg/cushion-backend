cd659c595d1b4815ba9a8af8f85e7a2dd0b297c7
nataliafuentes <nataliaf1412@gmail.com>
2026-05-27 12:25:11 -0500

    image-dto-thumbnail

--- src/main/java/com/cushion/cushion_backend/dto/ProductImageDTO.java
+++ src/main/java/com/cushion/cushion_backend/dto/ProductImageDTO.java
@@ -1,10 +1,12 @@
 package com.cushion.cushion_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ProductImageDTO {
     private String imageUrl;
     private String altText;
+    @JsonProperty("isThumbnail")
     private boolean isThumbnail;
 }
